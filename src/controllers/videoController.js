48008a965b4090ebe513af7982a4bda3baf5eb4b
summereuna <summereuna@gmail.com>
2021-08-11 14:00:25 +0900

    Add pugjs

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,9 @@
 //비디오 첫 화면: 트렌딩 비디오 페이지
-export const trending = (req, res) => res.send("Home page Videos");
-export const search = (req, res) => res.send("Search");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res)  => res.render("watch", { pageTitle: "Watch Video" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit Video" });
 
-export const see = (req, res) => res.send("See Video");
-export const edit = (req, res) => res.send("Edit Video");
+export const search = (req, res) => res.send("search");
 export const deleteVideo= (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
 
--- src/server.js
+++ src/server.js
@@ -9,9 +9,11 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
 app.use(logger);
-
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
