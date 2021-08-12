9e4b7eb33529e2d1784e3fcaa16f244fb6abb13c
summereuna <summereuna@gmail.com>
2021-08-12 18:49:23 +0900

    Create videoSchema

--- src/db.js
+++ src/db.js
@@ -4,3 +4,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("❌ DB Error", error);
+//db.on("error", (error) => console.log("❌ DB Error", error));
+db.once("open", handleOpen);
+db.on("error", handleError);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import "./db";
-
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 
