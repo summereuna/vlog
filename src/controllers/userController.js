2871f7c130222fb823ad719c20a1536850c5418c
summereuna <summereuna@gmail.com>
2021-08-28 16:41:03 +0900

    Add Join page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,21 @@
-export const join = (req, res) =>
-  res.send("createAccount", { pageTitle: "Create Account" });
+import User from "../models/User";
 
-export const login = (req, res) => res.send("Login");
-export const see = (req, res) => res.send("See User");
-export const logout = (req, res) => res.send("Log Out");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("login");
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Log out");
+export const see = (req, res) => res.send("See User");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,15 +1,13 @@
 import express from "express";
 
 import { home, search } from "../controllers/videoController";
-import { join, login } from "../controllers/userController";
+import { getJoin, postJoin, login } from "../controllers/userController";
 
-// create router
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.get("/join", join);
+rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.get("/login", login);
 rootRouter.get("/search", search);
 
-//export router -> default로 루트 라우터 익스포트 하기
 export default rootRouter;
