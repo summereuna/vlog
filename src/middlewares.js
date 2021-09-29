d9557060e4d8f21fda75ae416edc56250f8ad256
summereuna <summereuna@gmail.com>
2021-09-29 15:17:24 +0900

    Add video upload

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,4 @@
-import multer from "multer";
+import multer, { MulterError } from "multer";
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -25,4 +25,16 @@ export const publicOnlyMiddleware = (req, res, next) => {
   }
 };
 
-export const uploadFiles = multer({ dest: "uploads/" });
+export const avatarUpload = multer({
+  dest: "uploads/avatars/",
+  limits: {
+    fileSize: 3000000,
+  },
+});
+
+export const videoUpload = multer({
+  dest: "uploads/videos/",
+  limits: {
+    fileSize: 30000000,
+  },
+});
